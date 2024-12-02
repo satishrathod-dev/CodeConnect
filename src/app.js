45c2b49a579aa0f2fc24fbbd5e107a3f1d9bfe24
Satish Rathod <satishrathod.dev@gmail.com>
2024-12-02 21:18:12 +0530

    playing with routes

--- src/app.js
+++ src/app.js
@@ -3,13 +3,32 @@ const express = require("express");
 
 // creating and express app name "app"
 const app = express();
-app.use("/test", (req, res) => {
-  res.send("Hey there!!");
+
+// GET API call
+app.get("/user/:userId/:name/:password", (req, res) => {
+  console.log(req.query);
+  console.log(req.params);
+  res.send({
+    name: "Satish",
+    age: 22,
+  });
 });
 
-app.use((req, res) => {
-  res.send("Hello World");
-});
+// // POST API call
+// app.post("/user", (req, res) => {
+//   console.log("Data saved in the Database");
+//   res.send("Data saved in the Database");
+// });
+
+// //  Delete API call
+// app.delete("/user", (req, res) => {
+//   res.send("User deleted successfully!");
+// });
+
+// //  This will match all the HTTP method API calls to /test
+// app.use("/test", (req, res) => {
+//   res.send("Hey there!!");
+// });
 
 // Listening on port 3000
 app.listen(3000, () => {
